7fba0e21d7f5f3a0695a4a26e4ecccf53b4a59d6
Eliott Vallee dore <evallee-@c1r4p4.42quebec.com>
2023-11-30 14:29:02 -0500

    eval

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: niceguy <niceguy@student.42.fr>            +#+  +:+       +#+        */
+/*   By: evallee- <evallee-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 12:57:02 by niceguy           #+#    #+#             */
-/*   Updated: 2023/11/28 12:57:23 by niceguy          ###   ########.fr       */
+/*   Updated: 2023/11/30 14:21:39 by evallee-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,15 @@
 int main( void ) {
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	Fixed c(4.3f);
+
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
+	std::cout << c << std::endl;
 	std::cout << Fixed::max( a, b ) << std::endl;
 	return 0;
 }
